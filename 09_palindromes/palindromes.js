6215437d21e5f929d3e9a992968c55fcfb23a412
tobuya <thomasobuya95@yahoo.com>
2022-03-14 16:21:32 +0300

    Solved and tested .js against .spec.js palindromes files

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (processedString.split("").reverse().join("") === processedString);
 };
 
 // Do not edit below this line
