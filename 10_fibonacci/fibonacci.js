8f50df50dd0d2482b6a0131626055313404e5342
tobuya <thomasobuya95@yahoo.com>
2022-03-14 21:42:40 +0300

    Solved and tested fibonacci.js against fibanacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    let a = 0;
+    let b = 1;
+    for (let i = 1; i < num; i++){
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
 };
 
 // Do not edit below this line
